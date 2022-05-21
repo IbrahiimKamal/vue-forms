0302d0eb65396333ecc6f5180d8f5d5ec1be72f9
Ibrahim Kamal <ebrahiim.kamal@gmail.com>
2022-05-21 23:13:25 +0200

    vue-forms

---
+++ src/components/RatingInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="selectOption('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="selectOption('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="selectOption('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+
+  methods: {
+    selectOption(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: purple;
+}
+
+.active button {
+  color: purple;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  margin: 0.5rem 0;
+}
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font: inherit;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
